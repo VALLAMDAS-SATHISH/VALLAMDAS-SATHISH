78921a3f29c3884c8db595d8710f5f8c7c539953
VALLAMDAS-SATHISH <100330601+VALLAMDAS-SATHISH@users.noreply.github.com>
2022-03-06 21:46:21 +0530

    Add files via upload

---
+++ phase1/assistedPractice/pack2/AccessSpecifiers4.java
@@ -0,0 +1,15 @@
+package pack2;
+import pack1.*;
+
+public class AccessSpecifiers4 {
+
+public static void main(String[] args) {
+		
+	PubaccessSpecifiers obj = new PubaccessSpecifiers(); 
+        obj.display();  
+		
+	}
+}
+
+	
+
